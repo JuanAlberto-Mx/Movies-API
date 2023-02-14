9b326f6c6ca0f410d06812e2a1209957c7c694fc
Juan-Alberto Hernández-Martínez <jahm.developer@gmail.com>
2023-02-13 23:49:11 -0600

    Add a controller class to manage the movies information.

---
+++ src/main/java/dev/jahm/movies/MovieController.java
@@ -0,0 +1,39 @@
+package dev.jahm.movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/v1/movies") // Establish a starting point to the controller
+public class MovieController {
+
+    @Autowired
+    private MovieService movieService;
+
+    /**
+     * Gets the list of movies existing in the collection.
+     * @return the list of movies.
+     */
+    @GetMapping
+    public ResponseEntity<List<Movie>> getAllMovies() {
+        return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
+    }
+
+    /**
+     * Gets a particular movie according to a specific id.
+     * @param indbId the id of the movie requested.
+     * @return the movie requested in case of existing.
+     */
+    @GetMapping("/{indbId}")
+    public ResponseEntity<Optional<Movie>> getMovie(@PathVariable String indbId) {
+        return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(indbId), HttpStatus.OK);
+    }
+}
