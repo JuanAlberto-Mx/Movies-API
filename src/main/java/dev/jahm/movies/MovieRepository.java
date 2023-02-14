1f2ea2b8b822dbae11852d9011f7394b7d4ebd9c
Juan-Alberto Hernández-Martínez <jahm.developer@gmail.com>
2023-02-13 23:34:21 -0600

    Add interface to specify the methods to get movies information

---
+++ src/main/java/dev/jahm/movies/MovieRepository.java
@@ -0,0 +1,23 @@
+package dev.jahm.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+/**
+ * The <code>MovieRepository</code> interface encapsulates the methods necessary
+ * to get information from the <code>Movie</code> collection establishing a direct
+ * relation with MongoDB.
+ */
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+
+    /**
+     * Gets a particular movie by specifying an id value.
+     * @param indbId the id of the movie.
+     * @return the information of the movie in case of existing.
+     */
+    Optional<Movie> findMovieByIndbId(String indbId);
+}
