577eaa7324d209c3d7c9c8fcee2145ccf15c639e
Juan-Alberto Hernández-Martínez <jahm.developer@gmail.com>
2023-02-13 23:43:36 -0600

    Add a wrapper class to access the MovieRepository interface methods

---
+++ src/main/java/dev/jahm/movies/MovieService.java
@@ -0,0 +1,36 @@
+package dev.jahm.movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * The <code>MovieService</code> class provides a set of wrapper methods
+ * to access the functionality of the <code>MovieRepository</code>
+ * interface and get the movies information directly from MongoDB.
+ */
+@Service
+public class MovieService {
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    /**
+     * Gets all the movies from the collection.
+     * @return the list of movies.
+     */
+    public List<Movie> allMovies(){
+        return movieRepository.findAll();
+    }
+
+    /**
+     * Gets a movie specified by an id.
+     * @param indbId the id of the movie.
+     * @return the movie requested.
+     */
+    public Optional<Movie> singleMovie(String indbId) {
+        return movieRepository.findMovieByIndbId(indbId);
+    }
+}
